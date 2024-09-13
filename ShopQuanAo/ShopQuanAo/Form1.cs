ee9ca0f1bb5e1e913a33e3f8f922dc5f09afed83
LeHuuThinh <2212467@dlu.edu.vn>
2024-09-13 10:49:11 +0700

    Add files via upload

---
+++ ShopQuanAo/ShopQuanAo/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopQuanAo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            grbSanPham.Visible = false;
+
+            grbHangHot.Visible = true;
+            grbVoucher.Visible = false;
+            
+            grbAbtUs.Visible = false;
+
+            grbDanhMuc.Visible = false;
+
+            grbCart.Visible = false;
+
+            grbDaThem.Visible = true;
+            grbDaMua.Visible = false;
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Gacha f = new Gacha(this);
+            f.Show();
+            
+        }
+        private void btnTrangChu_Click(object sender, EventArgs e)
+        {
+
+            grbAbtUs.Visible = false;
+            grbDanhMuc.Visible = false;
+            grbCart.Visible = false;
+            grbSanPham.Visible = false;
+            grbAd.Visible = true;
+            grbTrangChu.Visible = true;
+
+        }
+        private void btnHangHot_Click(object sender, EventArgs e)
+        {
+            grbVoucher.Visible = false;
+            grbHangHot.Visible = true;
+            
+
+        }
+
+        private void btnVoucher_Click(object sender, EventArgs e)
+        {
+            grbHangHot.Visible = false;
+            grbVoucher.Visible = true;
+            
+        }
+
+        private void btnAbtUs_Click(object sender, EventArgs e)
+        {
+            grbTrangChu.Visible = false;
+            grbDanhMuc.Visible = false;
+            grbCart.Visible = false;
+            grbSanPham.Visible = false;
+            grbAbtUs.Visible = true;
+            grbAd.Visible = false;
+        }
+
+        
+
+        private void btnSanPham_Click(object sender, EventArgs e)
+        {
+            grbAbtUs.Visible = false;
+            grbTrangChu.Visible = false;
+            grbCart.Visible = false;
+            grbDanhMuc.Visible = false;
+            grbAd.Visible = true;
+            grbSanPham.Visible = true;
+
+        }
+
+        private void btnDanhMuc_Click(object sender, EventArgs e)
+        {
+            grbAbtUs.Visible = false;
+            grbTrangChu.Visible = false;
+            grbCart.Visible = false;
+            grbSanPham.Visible = false;
+            grbAd.Visible = false;
+            grbDanhMuc.Visible = true;
+        }
+
+        private void btnCart_Click(object sender, EventArgs e)
+        {
+            grbAbtUs.Visible = false;
+            grbTrangChu.Visible = false;
+            grbSanPham.Visible = false;
+            grbDanhMuc.Visible = false;
+            grbAd.Visible = true;
+            grbCart.Visible = true;
+        }
+
+        private void btnCartThem_Click(object sender, EventArgs e)
+        {
+            grbDaMua.Visible = false;
+            grbDaThem.Visible = true;
+        }
+
+        private void btnCartMua_Click(object sender, EventArgs e)
+        {
+            grbDaThem.Visible = false;
+            grbDaMua.Visible = true;
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool isRed = false;
+
+        private void tmAd_Tick(object sender, EventArgs e)
+        {
+            if (isRed)
+            {
+                llbAd.BackColor = Color.Transparent; // Trở lại màu ban đầu
+                llbAd.LinkColor = Color.Blue;
+                lblKhauHieu.ForeColor = Color.Blue;
+            }
+            else
+            {
+                llbAd.BackColor = Color.Red; // Đổi sang màu đỏ
+                llbAd.LinkColor = Color.Yellow; // Đổi sang màu đỏ
+                lblKhauHieu.ForeColor = Color.Red;
+
+            }
+
+            // Đảo trạng thái isRed để tạo hiệu ứng nhấp nháy
+            isRed = !isRed;
+        }
+    }
+}
